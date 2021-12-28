7adbaacec241ccf4cbfddaaa2121aea31216a719
carlos <chlouzada@gmail.com>
2021-12-28 11:36:16 -0300

    consumer and producer apps

--- producer/index.js
+++ producer/index.js
@@ -1,1 +1,21 @@
-console.log("Consumer");
+console.log("producer");
+
+// import { KafkaClient, Producer } from "kafka-node";
+
+// const client = new KafkaClient({ kafkaHost: "0.0.0.0:9092" });
+
+// const producer = new Producer(client);
+
+// producer.on("ready", () => {
+//   const payloads = [{ topic: "topico-teste", messages: "hi" }];
+
+//   producer.send(payloads, function (err, data) {
+//     console.log(err);
+//     console.log(data);
+//   });
+// });
+
+// setInterval(() => {
+//   console.log(client)
+// }
+// , 1000);
