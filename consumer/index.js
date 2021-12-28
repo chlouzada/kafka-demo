973eee161d601562f183ca83bc4f6cf6e44874ae
carlos <chlouzada@gmail.com>
2021-12-28 13:49:53 -0300

    consumer app

--- consumer/index.js
+++ consumer/index.js
@@ -1,1 +1,20 @@
-console.log("Consumer");
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+const consumer = kafka.consumer({ groupId: "test-group" });
+
+await consumer.connect();
+await consumer.subscribe({ topic: "kafka-demo", fromBeginning: true });
+
+await consumer.run({
+  eachMessage: async ({ topic, partition, message }) => {
+    console.log({
+      topic,
+      value: message.value.toString(),
+    });
+  },
+});
